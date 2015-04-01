8672ecab739efee0c660b5cdb6dce800312d5489
XxNagibatorxX <Suwigor@yandex.ru>
2015-04-01 16:21:26 +0600

    3441

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -46,7 +46,7 @@ int RUCHKA;
 void setup() {
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
-  // Print a message to6 the 456435eLCD.
+  // Print a message to the 456435eLCD.
   lcd.print("RUCHKA");
   //("M");
   //lcd.print("e");
@@ -61,14 +61,13 @@ void setup() {
 void loop() {
   lcd.clear();
   lcd.home();
+  lcd.print("RUCHKA");
   RUCHKA=analogRead(A0);
-  map(RUCHKA,0, 1023, 0, 17);
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  for(int i=0; i<=RUCHKA; i++)
-  lcd.print("\xFF");
+  lcd.print(RUCHKA);
 
   
   delay(100);
